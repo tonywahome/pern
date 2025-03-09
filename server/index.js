8f30d93dc292319719a08f525d361810914219b4
tonywahome <a.wambugu@alustudent.com>
2025-03-10 00:30:46 +0200

    database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//ROUTES//
+
+//create a todo
+app.post("/todos", async (req, res) => {
+    try {
+        console.log(req.body);
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING *",
+            [description]
+        );
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+//get all todos
+
+//get a todo
+
+//update a todo
 
 app.listen(5000, () => {
     console.log('server is listening on port 5000');
